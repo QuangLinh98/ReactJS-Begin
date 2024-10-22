9a5cfc115084c6e3b7547d074bcaa409975f97dd
Nguyen Quang Linh <nguyenlinhak4@gmail.com>
2024-10-22 11:11:25 +0700

    Cách sử dụng biến số với JSX

--- src/component/MyComponent.jsx
+++ src/component/MyComponent.jsx
@@ -2,8 +2,26 @@
 //component = html + css + js
 //Trong thế giới của Javascript tất cả đều là function , để biến nó thành 1 component bắt buộc chúng ta cần return ra HTML
 const MyComponent = () => {
+
+    //const variable = "Quang Linh"; //String 
+    //const variable = 25   //number
+    //const variable = true   //boolean
+    //const variable = undefined   
+    //const variable = null   
+    // const variable = [1, 2, 3]
+    const variable = {
+        name: "Quang Linh",
+        age: 26
+    }
+
+
+
+
     return (
-        <div>Quang Linh</div>
+        //Nếu muốn in ra cả biến mà không cần chấm tới thuộc tính thì chúng ra cần convert sang kiẻu JSON
+        // <div>Quang Linh {JSON.stringify(variable)}</div>
+
+        <div>Quang Linh {variable.age}</div>
     )
 }
 
